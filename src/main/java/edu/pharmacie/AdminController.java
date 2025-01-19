bcddda803f337043462c17df78831c97fff838e4
Djamel Nait Sidenas <xamel.ns@gmail.com>
2025-01-19 01:17:53 -0500

    Handle view change

--- src/main/java/edu/pharmacie/AdminController.java
+++ src/main/java/edu/pharmacie/AdminController.java
@@ -1,16 +1,27 @@
 package edu.pharmacie;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
+import java.io.IOException;
+
 public class AdminController {
 
     @FXML
     private StackPane adminViewContainer;
+
+    private Node dashboardView;
+    private Node employeeView;
+    private Node drugView;
+    private Node customerView;
+
+    private Node currentView;
+
     @FXML
     private VBox buttonsList;
     @FXML
@@ -23,20 +34,53 @@ public class AdminController {
     private HBox customerMenuButton;
 
 
+
+
     private HBox selectedItem;
 
-    public void initialize(){
+    public void initialize() throws IOException {
         selectedItem = dashboardMenuButton;
+        addMenuButtonListeners();
+
+        dashboardView = loadView("views/dashboard-view.fxml");
+        employeeView = loadView("views/employee-view.fxml");
+        drugView = loadView("views/drug-view.fxml");
+        customerView = loadView("views/customer-view.fxml");
+        currentView = dashboardView;
+        adminViewContainer.getChildren().add(currentView);
+
+    }
+
+    private Node loadView(String path) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(path));
+        return loader.load();
+    }
+    private void addMenuButtonListeners(){
         for (Node node : buttonsList.getChildren()) {
             if (node instanceof HBox hBox && hBox.getStyleClass().contains("left-menu-button")) {
                 hBox.setOnMouseClicked(this::handleMouseClick);
             }
         }
     }
-
     private void handleMouseClick(MouseEvent event) {
-        handleMenuItemsClasses((HBox) event.getSource());
+        HBox clickedItem = (HBox) event.getSource();
+        String viewName = (String) clickedItem.getUserData();
+        handleMenuItemsClasses(clickedItem);
+        try {
+        Node view = (Node) getClass().getDeclaredField(viewName).get(this);
+        currentView = view;
+        showView(view);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
 
+
+
+    }
+
+    private void showView(Node view) {
+        adminViewContainer.getChildren().clear();
+        adminViewContainer.getChildren().add(view);
     }
 
     private void handleMenuItemsClasses(HBox clickedMenuItem){
