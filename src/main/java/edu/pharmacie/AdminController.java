cf508309f6e8418d71c933418000d7951022085c
Djamel Nait Sidenas <xamel.ns@gmail.com>
2025-01-19 00:29:55 -0500

    Add admin views parts

--- src/main/java/edu/pharmacie/AdminController.java
+++ src/main/java/edu/pharmacie/AdminController.java
@@ -4,14 +4,24 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 public class AdminController {
-    @FXML
-    private HBox dashboardMenuButton;
 
     @FXML
+    private StackPane adminViewContainer;
+    @FXML
     private VBox buttonsList;
+    @FXML
+    private HBox dashboardMenuButton;
+    @FXML
+    private HBox employeeMenuButton;
+    @FXML
+    private HBox drugMenuButton;
+    @FXML
+    private HBox customerMenuButton;
+
 
     private HBox selectedItem;
 
@@ -26,6 +36,7 @@ public class AdminController {
 
     private void handleMouseClick(MouseEvent event) {
         handleMenuItemsClasses((HBox) event.getSource());
+
     }
 
     private void handleMenuItemsClasses(HBox clickedMenuItem){
@@ -33,5 +44,6 @@ public class AdminController {
         selectedItem = clickedMenuItem;
         clickedMenuItem.getStyleClass().add("selected");
     }
+
 
 }
---
+++ src/main/java/edu/pharmacie/DrugController.java
@@ -0,0 +1,4 @@
+package edu.pharmacie;
+
+public class DrugController {
+}
